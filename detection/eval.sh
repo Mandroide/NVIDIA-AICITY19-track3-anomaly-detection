890bf3e67463cff9c120f9dd2e2f36ecc2c04e8c
Manuel Garcia <31109774+Mandroide@users.noreply.github.com>
2021-06-11 17:13:43 -0400

    Double quote to prevent globbing

--- detection/eval.sh
+++ detection/eval.sh
@@ -18,10 +18,10 @@ MV2_USE_CUDA=1
 MV2_ENABLE_AFFINITY=0
 MV2_SMP_USE_CMA=0
 
-srun --partition=${PARTITION} --mpi=pmi2 --gres=gpu:1 -n1 --ntasks-per-node=1 --job-name=${TASKNAME} \
+srun --partition="${PARTITION}" --mpi=pmi2 --gres=gpu:1 -n1 --ntasks-per-node=1 --job-name="${TASKNAME}" \
   /mnt/lustre/weijiayi/sensenet/example/build/tools/caffe demo_test \
-  --model=./test.prototxt --weights=${WEIGHTS} \
+  --model=./test.prototxt --weights="${WEIGHTS}" \
   --config=./config.json \
   --test_image_list=/mnt/lustre/weijiayi/AIC19/aic19-track3/test_bg.txt \
   --image_dir=/mnt/lustre/weijiayi/AIC19/aic19-track3/ \
-  --vis_img=1 --out_dir=./vis_res 2>&1 | tee ./log/log_${TASKNAME}
+  --vis_img=1 --out_dir=./vis_res 2>&1 | tee ./log/log_"${TASKNAME}"
